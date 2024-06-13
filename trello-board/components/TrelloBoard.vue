502c3dbd95952ffeb6cfd400ac1e980e8208a906
Luiz Sampaio <luiznoel2006@gmail.com>
2024-06-13 07:36:26 -0400

    updating colllumn addition

--- trello-board/components/TrelloBoard.vue
+++ trello-board/components/TrelloBoard.vue
@@ -1,9 +1,9 @@
     <script lang="ts"  setup>
-    import { ref } from 'vue'
+
     import type {Column, Task } from "~~/types"
     import { nanoid } from 'nanoid'
     import draggable from 'vuedraggable'
-    const columns = ref<Column[]>([
+    const columns = useLocalStorage<Column[]>("trelloBoard ",[
       {
         id: nanoid(),
         title: "Backlog",
@@ -36,22 +36,42 @@
     ])
 
 const alt = useKeyModifier("Alt")
+    const createColumn = ()=> {
+      const column: Column = {
+        id: nanoid(),
+        title: "",
+        tasks:[]
+      }
+      columns.value.push(column);
+      nextTick(() => {
+        (
+          document.querySelector(".column:last-of-type .title-input"
+          ) as HTMLInputElement).focus()
+      })
+
+    }
     </script>
 <template>
-<div  >
+<div  class="flex items-start overflow-x-auto gap-4">
   <draggable
-  v-model="columns"
+  :v-model="columns"
   group="columns"
   handle=".drag-handle"
   item-key="id"
   animation="150"
-  class="flex gap-4 overflow-x-auto items-start"
+  class="flex gap-4  items-start"
   >
   <template #item="{element: column}: {element: Column}" >
-    <div  class="bg-gray-200 p-5 rounded min-w-[250px]">
+    <div  class="column bg-gray-200 p-5 rounded min-w-[250px]">
       <header class="font-bold mb-4">
         <DragHandle />
-        {{column.title}}
+        <input
+        class="title-input bg-transparent focus:bg-white rounded px-1 w-4/5"
+        @keyup.enter="($event.target as HTMLInputElement).blur()"
+        @keydown.backspace=" column.title === ''? columns = columns.filter( c=> c.id !== column.id) : null "
+        type="text"
+        v-model="column.title"
+        />
       </header>
       <draggable
         v-model="column.tasks"
@@ -75,6 +95,10 @@ const alt = useKeyModifier("Alt")
     </div>
   </template>
   </draggable>
+  <button
+  @click="createColumn"
+  class="bg-gray-200 whitespace-nowrap p-2 rounded opacity-50"
+  > + Add Another Column </button>
 
   </div>
 
